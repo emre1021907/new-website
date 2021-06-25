fa1efc8a80c74ab7c8e885c9dd38ce44d1d0d217
emre kose <koseemre2@gmail.com>
2021-06-25 11:52:57 +0300

    layouts added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // npm install -g nodemon 
 // nodemon app.js ile açarsak yaptığımız değişiklikler anında web sitesine yansır.
 // npm remove modüladı  --> modülü siler
-
+// express-ejs-layouts --save  ejs modülündeki sayfa şablonlarını içerir
 
 var http = require('http');
 var fs= require('fs');
@@ -14,8 +14,12 @@ var express = require('express');
 const path = require('path');
 var app = express()
 
+var ejslayout = require('express-ejs-layouts')
+
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'./app_server/views'))
+
+app.use(ejslayout);
 
 app.use('/public',express.static(path.join(__dirname,'public')));
 
--- app_server/controller/elektronikcontroller.js
+++ app_server/controller/elektronikcontroller.js
@@ -2,13 +2,12 @@ const path = require('path');
 
 
 
-module.exports.index = function(req,res){
+module.exports.index = function(req,res) {
+    res.render('elektronik')
+}
+
+module.exports.bilgisayar = function(req,res){
         
     var kisiler = ['ahmet','mehmet','ali','veli']
     res.render('bilgisayar',{mesaj:"merhaba ben controller",adlar : kisiler})
 };
-
-module.exports.bilgisayar = function(req,res){
-    res.sendFile(path.join(__dirname,'../../login.html'));
-    };
-
